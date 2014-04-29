9ebb68c19b7eb819bfd86e8dbd5030949c407e76
Alessandro Piras <laynor@gmail.com>
2014-04-30 00:26:12 +0200

    Move ExampleComponents to resources

--- ExampleMain/ExampleMain.pro
+++ ExampleMain/ExampleMain.pro
@@ -5,11 +5,10 @@ QT += qml quick
 SOURCES += main.cpp
 
 RESOURCES += qml.qrc
-INSTALLS += ../Modules
 
-QML2_IMPORT_PATH=../Modules
+QML2_IMPORT_PATH=Modules
 # Additional import path used to resolve QML modules in Qt Creator's code model
-QML_IMPORT_PATH =../Modules
+QML_IMPORT_PATH =Modules
 
 # Default rules for deployment.
 include(deployment.pri)
--- ExampleMain/main.cpp
+++ ExampleMain/main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.addImportPath(QStringLiteral("/home/ale/src/qt/example/Modules/"));
+    //engine.addImportPath(QStringLiteral("../Modules/"));
+    engine.addImportPath(QStringLiteral(":/Modules/"));
     engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
 
     return app.exec();
