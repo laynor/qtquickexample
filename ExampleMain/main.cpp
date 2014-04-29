3062e4f6d408b42cd083967e30fcd074b5ed4058
Alessandro Piras <laynor@gmail.com>
2014-04-29 23:51:39 +0200

    Add importPath to the qml engine.

--- ExampleMain/main.cpp
+++ ExampleMain/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    engine.addImportPath(QStringLiteral("/home/ale/src/qt/example/Modules/"));
     engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
 
     return app.exec();
